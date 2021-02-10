5b75be364fad56ab60d716984da18b18fe130936
miloszzaj <zajaczkowski.milosz@gmail.com>
2021-02-10 21:55:24 +0100

    typing challenge

--- main.js
+++ main.js
@@ -8,21 +8,27 @@ const charsNumber = 10;
 
 const codesGenerator = () => {
     section.innerHTML = "";
-    for (let i = 0; i < codesNumber; i++) {
-        // const code = "text"; 
-        let code = "";
+    let code = '';
+for (let i = 0; i < codesNumber; i++) {
+        // const code = "text";
         for (let i = 0; i < charsNumber; i++) {
             const index = Math.floor(Math.random() * chars.length);
-code += chars[index];
+            code += chars[index];
         }
-    
         const div = document.createElement('div');
-        div.textContent = code;
-        section.appendChild(div);
+    const typing = setInterval(() => textTyping(div, code), 500);
+    console.log(typing);
+    if (code.length === 10) clearInterval(typing);
+    section.appendChild(div);
+    
     }
 }
+const textTyping = (div, code) => {
+    let indexText = 0;
+            div.textContent += code[indexText];
+    indexText++;
+        // if (indexText == code.length) clearInterval(typing);
 
-
-
+}
 btn.addEventListener('click', codesGenerator)
 
